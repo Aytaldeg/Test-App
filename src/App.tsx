cc39f2d70bc785a72aa98288568866481d5669a6
Aytaldeg <91862666+Aytaldeg@users.noreply.github.com>
2023-05-03 14:21:40 +0900

    # Added statistics

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import './App.css';
-import './components/styles/main.scss';
-import Header from './components/Header/Header';
-import Main from './components/Main/Main';
-import Strategy from './components/Strategy/Strategy';
-import Retaily from './components/Retaily/Retaily';
+import React from "react";
+import "./App.css";
+import "./components/styles/main.scss";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Strategy from "./components/Strategy/Strategy";
+import Retaily from "./components/Retaily/Retaily";
+import Statistics from "./components/Statistics/Statistics";
 
 function App() {
   return (
     <div className="App">
-     <Header />
-     <Main />
-     <Strategy />
-     <Retaily />
+      <Header />
+      <Main />
+      <Strategy />
+      <Retaily />
+      <Statistics />
     </div>
   );
 }
---
+++ src/components/Statistics/Statistics.tsx
@@ -0,0 +1,16 @@
+import "../styles/main.scss";
+
+function Statistics() {
+  return (
+    <div className="statistics">
+      <div className="wrapper">
+        <div className="statistics-title">Сохраняйте время и деньги для вашего бизнеса</div>
+        <div className="statistics-data">
+         
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Statistics;
