db813a16400239506be2edeb82dd74cb3a4da55a
Aytaldeg <91862666+Aytaldeg@users.noreply.github.com>
2023-05-11 16:01:00 +0900

    # FAQ

--- src/components/Pages/Partner/FAQ/FAQ.tsx
+++ src/components/Pages/Partner/FAQ/FAQ.tsx
@@ -1,33 +1,33 @@
 function FAQ() {
   return (
-    <div className="faq">
+    <div className="pfaq">
       <div className="wrapper">
-        <div className="faq-title">F.A.Q</div>
-        <div className="faq-content">
-          <div className="faq-leftSide">
-            <div className="faq-line">
-              <div className="faq-dotup" />
-              <div className="faq-straight" />
-              <div className="faq-dotdown" />
+        <div className="pfaq-title">F.A.Q</div>
+        <div className="pfaq-content">
+          <div className="pfaq-leftSide">
+            <div className="pfaq-line">
+              <div className="pfaq-dotup" />
+              <div className="pfaq-straight" />
+              <div className="pfaq-dotdown" />
             </div>
-            <div className="faq-upfirst">
-              <div className="faq-ftext">
-                <div className="faq-titletext">
+            <div className="pfaq-upfirst">
+              <div className="pfaq-ftext">
+                <div className="pfaq-titletext">
                   Взимается ли плата за использование системы Retaily?
                 </div>
-                <div className="faq-text">
+                <div className="pfaq-text">
                   Использование мобильного приложения и сайта для пользователей
                   (торговых точек, контрагентов и т.д.) бесплатное.
                 </div>
               </div>
 
-              <div className="faq-downfirst">
-                <div className="faq-ftext">
-                  <div className="faq-titletext">
+              <div className="pfaq-downfirst">
+                <div className="pfaq-ftext">
+                  <div className="pfaq-titletext">
                     Возможно ли установить персональные цены для разных
                     контрагентов?
                   </div>
-                  <div className="faq-text">
+                  <div className="pfaq-text">
                     Цены для контрагентов в системе устанавливаются и
                     отображаются согласно условиям заключенных договоров с
                     поставщиком.
@@ -37,31 +37,31 @@ function FAQ() {
             </div>
           </div>
 
-          <div className="faq-rightSide">
-            <div className="faq-line">
-              <div className="faq-dotup" />
-              <div className="faq-straight" />
-              <div className="faq-dotdown" />
+          <div className="pfaq-rightSide">
+            <div className="pfaq-line">
+              <div className="pfaq-dotup" />
+              <div className="pfaq-straight" />
+              <div className="pfaq-dotdown" />
             </div>
-            <div className="faq-upsecond">
-              <div className="faq-ftext">
-                <div className="faq-titletext">
+            <div className="pfaq-upsecond">
+              <div className="pfaq-ftext">
+                <div className="pfaq-titletext">
                   Какое время занимает подключение к системе Retaily и сколько
                   она стоит?
                 </div>
-                <div className="faq-text">
+                <div className="pfaq-text">
                   Скорость подключения к системе от 14 дней в зависимости от
                   дополнительных настроек. Плата за подключение и дополнительные
                   настройки интерфейса не взимается.
                 </div>
               </div>
 
-              <div className="faq-downsecond">
-                <div className="faq-ftext">
-                  <div className="faq-titletext">
+              <div className="pfaq-downsecond">
+                <div className="pfaq-ftext">
+                  <div className="pfaq-titletext">
                     Как загружать фото продукции?
                   </div>
-                  <div className="faq-text">
+                  <div className="pfaq-text">
                     Фотографии продукции можно загрузить как в личном кабинете,
                     так и через обмен данными с 1С предприятия.
                   </div>
