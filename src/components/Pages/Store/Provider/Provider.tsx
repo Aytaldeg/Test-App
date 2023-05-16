9419ba16fe9d41b41e899f864cc803bdbeed2481
Aytaldeg <91862666+Aytaldeg@users.noreply.github.com>
2023-05-16 13:03:12 +0900

    # End store mobile

--- src/components/Pages/Store/Provider/Provider.tsx
+++ src/components/Pages/Store/Provider/Provider.tsx
@@ -3,59 +3,59 @@ import { isMobile } from "react-device-detect";
 
 function Provider() {
   return (
-    <div className="pprovider" id="provider">
-      <div className="pprovider-bg"></div>
+    <div className="sprovider" id="provider">
+      <div className="sprovider-bg"></div>
       <div className="wrapper">
         {isMobile ? (
           <Fragment>
             {" "}
-            <div className="pprovider-title">Как сделать заказ?</div>
-            <div className="ppprovider-maincont">
-              <div className="pprovider-contentup">
-                <div className="pprovider-content">
-                  <div className="pprovider-second">
-                    <div className="pprovider-line" />
-                    <div className="pprovider-dotsprov">
-                      <div className="pprovider-dotprov_active" />
-                      <div className="pprovider-dotprov" />
-                      <div className="pprovider-dotprov" />
-                      <div className="pprovider-dotprov" />
-                      <div className="pprovider-dotprov" />
+            <div className="sprovider-title">Как сделать заказ?</div>
+            <div className="sprovider-maincont">
+              <div className="sprovider-contentup">
+                <div className="sprovider-content">
+                  <div className="sprovider-second">
+                    <div className="sprovider-line" />
+                    <div className="sprovider-dotsprov">
+                      <div className="sprovider-dotprov_active" />
+                      <div className="sprovider-dotprov" />
+                      <div className="sprovider-dotprov" />
+                      <div className="sprovider-dotprov" />
+                      <div className="sprovider-dotprov" />
                     </div>
                   </div>
 
-                  <div className="pprovider-first">
-                    <div className="pprovider-numbers1">
-                      <div className="pprovider-number">1</div>
-                      <div className="pprovider-text">
+                  <div className="sprovider-first">
+                    <div className="sprovider-numbers1">
+                      <div className="sprovider-number">1</div>
+                      <div className="sprovider-text">
                         Зарегистрируйтесь на сайте или <br />
                         установите мобильное <br />
                         приложение
                       </div>
                     </div>
-                    <div className="pprovider-num">
-                      <div className="pprovider-numbers">
-                        <div className="pprovider-numbertwo">2</div>
-                        <div className="pprovider-texttwo">
+                    <div className="sprovider-num">
+                      <div className="sprovider-numbers">
+                        <div className="sprovider-numbertwo">2</div>
+                        <div className="sprovider-texttwo">
                           Выберите интересующего <br />
                           поставщика и, при <br />
                           необходимости, заключите <br />
                           договор поставки
                         </div>
                       </div>
-                      <div className="pprovider-num2">
-                        <div className="pprovider-numbers">
-                          <div className="pprovider-number">3</div>
-                          <div className="pprovider-text">
+                      <div className="sprovider-num2">
+                        <div className="sprovider-numbers">
+                          <div className="sprovider-number">3</div>
+                          <div className="sprovider-text">
                             Выберите товарную позицию из <br />
                             каталога и добавьте в корзину <br />
                             необходимое количество
                           </div>
                         </div>
-                        <div className="pprovider-num3">
-                          <div className="pprovider-numbers">
-                            <div className="pprovider-number">4</div>
-                            <div className="pprovider-text">
+                        <div className="sprovider-num3">
+                          <div className="sprovider-numbers">
+                            <div className="sprovider-number">4</div>
+                            <div className="sprovider-text">
                               Нажмите кнопку Оформить <br /> заявку и ожидайте
                               доставку от <br />
                               Поставщика
@@ -66,10 +66,7 @@ function Provider() {
                     </div>
                   </div>
                 </div>
-                <div className="ppprovider-ccontentdown">
-                  {" "}
-                  <div className="pprovider-second-bbutton">ПОДРОБНЕЕ</div>
-                </div>
+                <div className="sprovider-bbutton">ПОДРОБНЕЕ</div>
               </div>
             </div>
           </Fragment>
